552ea91f6b4032a7478660b0bfcc07fbe3a328ba
Javier Biselli <javierbiselli@gmail.com>
2022-04-15 15:32:51 -0300

    BaSP-W05-2022: First block done

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,28 @@
+console.log(`01-Variables and operators`)
+
+console.log(`Exercise A`);
+
+var num1 = 5
+var num2 = 8
+
+var result = num1 + num2
+
+console.log(`The result is: ${result}`)
+
+console.log(`Exercise B`);
+
+var string1 = `My name is `
+var string2 = `Javier`
+
+var result2 = string1 + string2
+
+console.log(`Strings linked together: ${result2}`)
+
+console.log (`Exercise C`)
+
+var stringLenght1 = `My last name is `
+var stringLenght2 = `Biselli`
+
+var stringLenghtTotal = stringLenght1.length + stringLenght2.length
+
+console.log (`The string lenght is: ${stringLenghtTotal}`)
