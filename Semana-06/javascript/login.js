c2647d595110b5071945528e3fdd5141227ee3f7
Javier Biselli <javierbiselli@gmail.com>
2022-04-21 16:49:31 -0300

    BaSP-W06-2022: Added js selectors

---
+++ Semana-06/javascript/login.js
@@ -0,0 +1,4 @@
+var emailInputLogin = document.querySelector('input[type="email"]');
+var passwordInputLogin = document.querySelector('input[type="password"]');
+var loginErrors = document.getElementsByClassName('login-error');
+var submitInputLogin = document.querySelector('input[type="submit"]');
