d4f29cc795c0937abc01b4f6078d71d29a695771
Javier Biselli <javierbiselli@gmail.com>
2022-04-23 23:01:46 -0300

    BaSP-W06-2022: Unfinished sign-up

--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -1,4 +1,5 @@
-var nameInputSignup = document.querySelector('input[name="name"]');
+// Selectors
+var nameInputSignup = document.querySelector('input[name="sign-upname"]');
 var lastnameInputSignup = document.querySelector('input[name="lastname"]');
 var dniInputSignup = document.querySelector('input[name="dni"]');
 var dateInputSignup = document.querySelector('input[name="date-of-birth"]');
@@ -6,8 +7,11 @@ var telInputSignup = document.querySelector('input[name="tel"]');
 var adressInputSignup = document.querySelector('input[name="adress"]');
 var locationInputSignup = document.querySelector('input[name="location"]');
 var postalCodeInputSignup = document.querySelector('input[name="postal-code"]');
-var emailInputSignup = document.querySelector('input[name="email"]');
+var emailInputSignup = document.querySelector('input[name="signup-email"]');
 var passwordInputSignup = document.querySelector('input[name="password"]');
 var repeatPasswordInputSignup = document.querySelector('input[name="repeat-password"]');
 var signupErrors = document.getElementsByClassName('signup-error');
 var submitInputSignup = document.querySelector('input[type="submit"]');
+
+// Name validation
+
